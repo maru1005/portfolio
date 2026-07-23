f1896e094b81b1a868bb4784736ca4f723b3c9b7
maruyama <maruyama@maruyamanoMacBook-Air.local>
2026-07-23 23:41:22 +0900

    feat:About section実装

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-    <section id="about" className="min-h-screen bg-orange-50">
-      About
+    <section
+      id="about"
+      className=" bg-amber-50 flex flex-col items-center justify-center gap-6 px-8 md:px-16 py-24"
+    >
+      <h2 className="text-4xl font-bold flex items-center gap-3">
+        About Meow🐾
+      </h2>
+      <p className="text-base leading-relaxed">
+        猫大好きエンジニア(見習い中)。
+        <br />
+        プログラミングスクール卒業後、個人開発を通して学習を続けています。
+      </p>
+      <a
+        href="https://github.com/maru1005"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block"
+      >
+        <Image
+          src="/GitHub_Lockup_Black.svg"
+          alt="GitHub"
+          width={128}
+          height={28}
+        />
+      </a>
     </section>
   );
 }
